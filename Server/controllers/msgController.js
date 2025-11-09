de3c1fff9e189c59b43cb868c1186d5f3c6d3340
PranavThorat1432 <pranavthorat95@gmail.com>
2025-11-09 17:54:49 +0530

    Msg Controller

---
+++ Server/controllers/msgController.js
@@ -0,0 +1,74 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
+import Conversation from "../models/ConversationModel.js";
+import Message from "../models/MsgModel.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
+
+
+export const sendMsg = async (req, res) => {
+    try {
+        const sender = req.userId;
+        const {receiver} = req.params;
+        const {message} = req.body;
+
+        let image;
+        if(req.file) {
+            image = await uploadOnCloudinary(req.file.path);
+        }
+
+        let conversation = await Conversation.findOne({
+            participants: {$all: [sender, receiver]}
+        });
+
+        let newMsg = await Message.create({
+            sender, receiver, message, image
+        });
+
+        if(!conversation) {
+            conversation = await Conversation.create({
+                participants: [sender, receiver],
+                messages: [newMsg._id]
+            });
+
+        } else {
+            conversation.messages.push(newMsg._id);
+            await conversation.save();
+        }
+        
+        const receiverSocketId = getReceiverSocketId(receiver);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit('newMsg', newMsg)
+        }
+
+        return res.status(201).json(newMsg);
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: `Send Msg Error: ${error}`
+        });
+    }
+};
+
+
+export const getMsgs = async (req, res) => {
+    try {
+        const sender = req.userId;
+        const {receiver} = req.params;
+
+        let conversation = await Conversation.findOne({
+            participants: {$all: [sender, receiver]}
+        }).populate('messages');
+
+        if(!conversation) {
+            return res.status(400).json({
+                message: 'Conversation not found'
+            });
+        }
+
+        return res.status(200).json(conversation?.messages);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: `Get Msg Error: ${error}`
+        });
+    }
+};
