d86a71ba9b6d22b7d3c3cfdea85f564261aefdcf
PranavThorat1432 <pranavthorat95@gmail.com>
2025-11-12 18:48:27 +0530

    Fix

--- Server/server.js
+++ Server/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
     const clientPath = path.resolve(__dirname, '../Client/dist');
     app.use(express.static(clientPath));
 
-    app.get('/*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(clientPath, 'index.html'));
     });
 }
